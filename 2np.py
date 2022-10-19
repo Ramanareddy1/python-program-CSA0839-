6397c8381d5bcd00c3885c3ea0f83512afec0d49
Ramanareddy1 <112605813+Ramanareddy1@users.noreply.github.com>
2022-10-19 14:45:14 +0530

    revision week program-4

---
+++ 2np.py
@@ -0,0 +1,18 @@
+if _ _name_ _ == '_ _main_ _':
+    T = int(input())
+    E = list(map(int,
+input().split()))
+    L = list(map(int,
+input().split()))
+    current_presence = 0
+    max_presence = 0
+    for i in range(T):
+        current_presence +=
+E[i] - L[i]
+        if max_presence<
+current_presence:
+            max_presence =
+current_presence
+    print (max_presence, end =
+'')
+    
